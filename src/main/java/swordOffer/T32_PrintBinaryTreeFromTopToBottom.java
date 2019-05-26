7dd05a22d26138b3294b539d6bb6355c0bbc8f5f
songxiaocang <1844367805@qq.com>
2019-05-26 20:49:38 +0800

    print binary tree from top to bottom

---
+++ src/main/java/swordOffer/T32_PrintBinaryTreeFromTopToBottom.java
@@ -0,0 +1,53 @@
+package swordOffer;
+
+import util.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author: Songxc
+ * @Date: 20:42 2019/5/26
+ * @Description: 从上到下打印二叉树（之字形）
+ *  思路：
+ *   层序遍历，可以使用链表实现，也可以使用双栈实现，下面仅贴出链表实现。
+ */
+public class T32_PrintBinaryTreeFromTopToBottom {
+    //链表
+    public static void printBinaryTree(TreeNode<Integer> root){
+        Queue<TreeNode<Integer>> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode<Integer> node = queue.poll();
+            System.out.print(node.val + " ");
+
+            if(node.left!=null){
+                queue.offer(node.left);
+            }
+            if(node.right!=null){
+                queue.offer(node.right);
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        /**
+         *       8
+         *      /\
+         *    2   4
+         *   /\  /\
+         *  1 5 6 9
+         */
+        TreeNode<Integer> root = new TreeNode<>(8);
+        root.left = new TreeNode<>(2);
+        root.right = new TreeNode<>(4);
+        root.left.left = new TreeNode<>(1);
+        root.left.right = new TreeNode<>(5);
+        root.right.left = new TreeNode<>(6);
+        root.right.right = new TreeNode<>(9);
+
+        printBinaryTree(root);
+    }
+}
