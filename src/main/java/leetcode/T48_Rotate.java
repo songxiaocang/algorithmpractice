cc970530c5db3a019af1a480c148dd9901a8db74
songxiaocang <1844367805@qq.com>
2019-07-28 23:40:42 +0800

    rotate

---
+++ src/main/java/leetcode/T48_Rotate.java
@@ -0,0 +1,60 @@
+package leetcode;
+
+/**
+ * @Author: Songxc
+ * @Date: 23:39 2019/7/28
+ * @Description: 旋转图像
+ * 给定一个 n × n 的二维矩阵表示一个图像。
+ * <p>
+ * 将图像顺时针旋转 90 度。
+ * <p>
+ * 说明：
+ * <p>
+ * 你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要使用另一个矩阵来旋转图像。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定 matrix =
+ * [
+ * [1,2,3],
+ * [4,5,6],
+ * [7,8,9]
+ * ],
+ * <p>
+ * 原地旋转输入矩阵，使其变为:
+ * [
+ * [7,4,1],
+ * [8,5,2],
+ * [9,6,3]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-image
+ * <p>
+ * 思路：
+ * 先获得转置矩阵，然后将矩阵每行反转
+ */
+public class T48_Rotate {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length <= 0) {
+            return;
+        }
+        int n = matrix.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+}
