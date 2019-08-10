9dd6da55a17770e9de67f17a6ff5718061250a74
songxiaocang <1844367805@qq.com>
2019-08-10 17:05:49 +0800

    max profit

---
+++ src/main/java/leetcode/T121_MaxProfit.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+/**
+ * @Author: Songxc
+ * @Date: 17:02 2019/8/10
+ * @Description: 买卖股票的最佳时机
+ * 思路：
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * <p>
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ * <p>
+ * 注意你不能在买入股票前卖出股票。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ * 注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * <p>
+ * 示例 2:
+ * <p>
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+ * <p>
+ * 思路：
+ * 一次遍历（动态规划）
+ * 使用两个中间变量minPrice和maxProfit记录最小的谷值和最大的利润。
+ * 时间复杂度0(n) 空间复杂度0(1)
+ */
+public class T121_MaxProfit {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
