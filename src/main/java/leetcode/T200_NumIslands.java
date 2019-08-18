34065797b5de2cf7349adf4506ba725edb50b51f
songxiaocang <1844367805@qq.com>
2019-08-18 12:55:08 +0800

    num is lands

---
+++ src/main/java/leetcode/T200_NumIslands.java
@@ -0,0 +1,210 @@
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author: Songxc
+ * @Date: 12:45 2019/8/18
+ * @Description: 岛屿数量
+ *  给定一个由 '1'（陆地）和 '0'（水）组成的的二维网格，计算岛屿的数量。一个岛被水包围，并且它是通过水平方向或垂直方向上相邻的陆地连接而成的。你可以假设网格的四个边均被水包围。
+ *
+ * 示例 1:
+ *
+ * 输入:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ *
+ * 输出: 1
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-islands
+ *
+ * 思路：
+ *  有三种解决办法：深度优先搜索， 广度优先搜索 ，并查集
+ *  1）深度优先搜索
+ *  线性扫描整个二维网格，如果一个结点包含 1，则以其为根结点启动深度优先搜索。在深度优先搜索过程中，每个访问过的结点被标记为 0。计数启动深度优先搜索的根结点的数量，即为岛屿的数量。
+ *  时间复杂度 : O(M×N)，其中 M 和 N 分别为行数和列数。
+ * 空间复杂度 : 最坏情况下为 O(M×N)，此时整个网格均为陆地，深度优先搜索的深度达到 M×N。
+ *
+ *  2）广度优先搜索
+ *  线性扫描整个二维网格，如果一个结点包含 1，则以其为根结点启动广度优先搜索。将其放入队列中，并将值设为 0 以标记访问过该结点。迭代地搜索队列中的每个结点，直到队列为空。
+ *  时间复杂度 : O(M×N)，其中 M 和 N 分别为行数和列数。
+ * 空间复杂度 : O(min(M,N))，在最坏的情况下（全部为陆地），队列的大小可以达到 min(M，N)。
+ *
+ * 3）并查集
+ * 遍历二维网格，将竖直或水平相邻的陆地联结。最终，返回并查集数据结构中相连部分的数量。
+ * 时间复杂度：O(M×N)，其中 M 和 N 分别为行数和列数。注意当使用路径压缩和排名结合并实现并查集时，并操作只需要消耗常数时间。
+ * 空间复杂度：O(M×N)，这是并查集数据结构需要的空间。
+ *
+ */
+public class T200_NumIslands {
+    //深度优先搜索
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0';
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+    //广度优先搜索
+    public int numIslands2(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    grid[r][c] = '0'; // mark as visited
+                    Queue<Integer> queue = new LinkedList<>();
+                    queue.add(r * nc + c);
+                    while (!queue.isEmpty()) {
+                        int id = queue.remove();
+                        int row = id / nc;
+                        int col = id % nc;
+                        if (row - 1 >= 0 && grid[row-1][col] == '1') {
+                            queue.add((row-1) * nc + col);
+                            grid[row-1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row+1][col] == '1') {
+                            queue.add((row+1) * nc + col);
+                            grid[row+1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col-1] == '1') {
+                            queue.add(row * nc + col-1);
+                            grid[row][col-1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col+1] == '1') {
+                            queue.add(row * nc + col+1);
+                            grid[row][col+1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+
+        return num_islands;
+    }
+
+
+    //并查集
+    class UnionFind {
+        int count; // # of connected components
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) { // for problem 200
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+            parent = new int[m * n];
+            rank = new int[m * n];
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n; ++j) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                    rank[i * n + j] = 0;
+                }
+            }
+        }
+
+        public int find(int i) { // path compression
+            if (parent[i] != i) parent[i] = find(parent[i]);
+            return parent[i];
+        }
+
+        public void union(int x, int y) { // union with rank
+            int rootx = find(x);
+            int rooty = find(y);
+            if (rootx != rooty) {
+                if (rank[rootx] > rank[rooty]) {
+                    parent[rooty] = rootx;
+                } else if (rank[rootx] < rank[rooty]) {
+                    parent[rootx] = rooty;
+                } else {
+                    parent[rooty] = rootx; rank[rootx] += 1;
+                }
+                --count;
+            }
+        }
+
+        public int getCount() {
+            return count;
+        }
+    }
+
+    //并查集
+    public int numIslands3(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        UnionFind uf = new UnionFind(grid);
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    grid[r][c] = '0';
+                    if (r - 1 >= 0 && grid[r-1][c] == '1') {
+                        uf.union(r * nc + c, (r-1) * nc + c);
+                    }
+                    if (r + 1 < nr && grid[r+1][c] == '1') {
+                        uf.union(r * nc + c, (r+1) * nc + c);
+                    }
+                    if (c - 1 >= 0 && grid[r][c-1] == '1') {
+                        uf.union(r * nc + c, r * nc + c - 1);
+                    }
+                    if (c + 1 < nc && grid[r][c+1] == '1') {
+                        uf.union(r * nc + c, r * nc + c + 1);
+                    }
+                }
+            }
+        }
+
+        return uf.getCount();
+    }
+
+
+
+
+}
