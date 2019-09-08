44657e0100bac7ccf8ed1a1d82750951c01ff5a4
songxiaocang <1844367805@qq.com>
2019-09-08 09:29:53 +0800

    top k frequent

---
+++ src/main/java/leetcode/T347_TopKFrequent.java
@@ -0,0 +1,96 @@
+package leetcode;
+
+import java.util.*;
+
+/**
+ * @Author: Songxc
+ * @Date: 9:23 2019/9/8
+ * @Description: 前K个高频元素
+ * 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+ *
+ * 示例 1:
+ *
+ * 输入: nums = [1,1,1,2,2,3], k = 2
+ * 输出: [1,2]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/top-k-frequent-elements
+ *
+ * 思路：
+ *  主要有两种解法
+ *   1） 堆排序（小顶堆）
+ *    哈希表 + 优先队列
+ *     时间复杂度为0（nlogk）,空间复杂度为0（n）
+ *   2）桶排序 （推荐）
+ *   哈希表 + 数组
+ *    时间复杂度为0（n）， 空间复杂度为 0（n）
+ */
+public class T347_TopKFrequent {
+    //桶排序
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> res = new ArrayList();
+        // 使用字典，统计每个元素出现的次数，元素为键，元素出现的次数为值
+        HashMap<Integer,Integer> map = new HashMap();
+        for(int num : nums){
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        //桶排序
+        //将频率作为数组下标，对于出现频率不同的数字集合，存入对应的数组下标
+        List<Integer>[] list = new List[nums.length+1];
+        for(int key : map.keySet()){
+            // 获取出现的次数作为下标
+            int i = map.get(key);
+            if(list[i] == null){
+                list[i] = new ArrayList();
+            }
+            list[i].add(key);
+        }
+
+        // 倒序遍历数组获取出现顺序从大到小的排列
+        for(int i = list.length - 1;i >= 0 && res.size() < k;i--){
+            if(list[i] == null) continue;
+            res.addAll(list[i]);
+        }
+        return res;
+    }
+
+    //堆排序
+    public List<Integer> topKFrequent2(int[] nums, int k) {
+        // 使用字典，统计每个元素出现的次数，元素为键，元素出现的次数为值
+        HashMap<Integer,Integer> map = new HashMap();
+        for(int num : nums){
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        // 遍历map，用最小堆保存频率最大的k个元素
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer a, Integer b) {
+                return map.get(a) - map.get(b);
+            }
+        });
+        for (Integer key : map.keySet()) {
+            if (pq.size() < k) {
+                pq.add(key);
+            } else if (map.get(key) > map.get(pq.peek())) {
+                pq.remove();
+                pq.add(key);
+            }
+        }
+        // 取出最小堆中的元素
+        List<Integer> res = new ArrayList<>();
+        while (!pq.isEmpty()) {
+            res.add(pq.remove());
+        }
+        return res;
+    }
+
+}
