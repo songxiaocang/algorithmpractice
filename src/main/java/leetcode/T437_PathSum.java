f1fbc565b95da7df908418cb2e00a6216ec2639c
songxiaocang <1844367805@qq.com>
2019-09-08 19:58:53 +0800

    path sum

---
+++ src/main/java/leetcode/T437_PathSum.java
@@ -0,0 +1,66 @@
+package leetcode;
+
+import util.TreeNode;
+
+/**
+ * @Author: Songxc
+ * @Date: 19:41 2019/9/8
+ * @Description: 路径总和III
+ * 给定一个二叉树，它的每个结点都存放着一个整数值。
+ *
+ * 找出路径和等于给定数值的路径总数。
+ *
+ * 路径不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+ *
+ * 二叉树不超过1000个节点，且节点数值范围是 [-1000000,1000000] 的整数。
+ *
+ * 示例：
+ *
+ * root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+ *
+ *       10
+ *      /  \
+ *     5   -3
+ *    / \    \
+ *   3   2   11
+ *  / \   \
+ * 3  -2   1
+ *
+ * 返回 3。和等于 8 的路径有:
+ *
+ * 1.  5 -> 3
+ * 2.  5 -> 2 -> 1
+ * 3.  -3 -> 11
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-sum-iii
+ *
+ * 思路；
+ *  递归
+ *
+ *   该题与swordOffer T34_findPath稍微有点不同，T34是要从根节点触发
+ */
+public class T437_PathSum {
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null){
+            return 0;
+        }
+
+        return path(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
+    }
+
+    public int path(TreeNode root, int sum){
+        if(root == null){
+            return 0;
+        }
+
+        int res = 0;
+        if((int)root.val == sum){
+            res += 1;
+        }
+
+        res += path(root.left, sum - (int)root.val);
+        res += path(root.right, sum - (int)root.val);
+        return res;
+    }
+}
