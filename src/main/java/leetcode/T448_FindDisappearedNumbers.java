8fe0763357af5bfbb9a0e5e1ce3823e57a30b569
songxiaocang <1844367805@qq.com>
2019-09-11 23:47:15 +0800

    find disappeared num

---
+++ src/main/java/leetcode/T448_FindDisappearedNumbers.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: Songxc
+ * @Date: 23:41 2019/9/11
+ * @Description: 找到所有数组中消失的数字
+ * 给定一个范围在  1 ≤ a[i] ≤ n ( n = 数组大小 ) 的 整型数组，数组中的元素一些出现了两次，另一些只出现一次。
+ * 找到所有在 [1, n] 范围之间没有出现在数组中的数字。
+ * 您能在不使用额外空间且时间复杂度为O(n)的情况下完成这个任务吗? 你可以假定返回的数组不算在额外空间内。
+ * 示例:
+ * 输入:
+ * [4,3,2,7,8,2,3,1]
+ * 输出:
+ * [5,6]
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-all-numbers-disappeared-in-an-array
+ *
+ * 思路：
+ * 异或运算
+ * 时间复杂度为0（n）， 空间复杂度为0（1）
+ */
+public class T448_FindDisappearedNumbers {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            while (nums[nums[i] - 1] != nums[i]) {
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                res.add(i + 1);
+            }
+        }
+        return res;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        if (index1 == index2) {
+            return;
+        }
+        nums[index1] = nums[index1] ^ nums[index2];
+        nums[index2] = nums[index1] ^ nums[index2];
+        nums[index1] = nums[index1] ^ nums[index2];
+    }
+
+
+}
